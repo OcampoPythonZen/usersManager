b16faac659b620301500fb70b0cfc2c8f327ee66
edgar.ocampo <edgar.ocampo@rappi.com>
2022-11-23 21:42:25 -0600

    batch class with class connector both separated

--- src/main/java/edo/mex/gob/gui/MainGUI.java
+++ src/main/java/edo/mex/gob/gui/MainGUI.java
@@ -2,12 +2,15 @@ package edo.mex.gob.gui;
 
 
 import edo.mex.gob.gui.home.HomeGUI;
-import edo.mex.gob.gui.menu.MenuGUI;
+import edo.mex.gob.repository.BatchSqlTables;
+import java.sql.SQLException;
 
 public class MainGUI {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         //HomeGUI main = new HomeGUI();
-        MenuGUI menu = new MenuGUI();
+        //MenuGUI menu = new MenuGUI();
+        BatchSqlTables sql = new BatchSqlTables();
+
     }
 }
