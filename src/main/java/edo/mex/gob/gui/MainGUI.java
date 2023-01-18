cc782810758748d3f07d1de77a654f7bd9137a1d
edgar.ocampo <edgar.ocampo@rappi.com>
2023-01-18 15:33:12 -0600

    email sender class done

--- src/main/java/edo/mex/gob/gui/MainGUI.java
+++ src/main/java/edo/mex/gob/gui/MainGUI.java
@@ -2,18 +2,13 @@ package edo.mex.gob.gui;
 
 
 import edo.mex.gob.gui.assignment.AssignmentFrame;
-import edo.mex.gob.gui.course.CourseGUI;
-import edo.mex.gob.gui.home.HomeGUI;
-import edo.mex.gob.gui.menu.MenuGUI;
-import edo.mex.gob.gui.user.UserGUI;
-import edo.mex.gob.repository.BatchSqlTables;
 import java.sql.SQLException;
 
 public class MainGUI {
 
     public static void main(String[] args) throws SQLException {
-        MenuGUI menu = new MenuGUI();
-        //AssignmentFrame af = new AssignmentFrame();
+        //MenuGUI menu = new MenuGUI();
+        AssignmentFrame af = new AssignmentFrame();
         //CourseGUI c = new CourseGUI();
         //HomeGUI main = new HomeGUI();
         //UserGUI menu = new UserGUI();
