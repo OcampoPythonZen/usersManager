e7db35cda1d50ea7c2c99df348eef879bb4ad324
edgar.ocampo <edgar.ocampo@rappi.com>
2023-01-29 01:47:46 -0600

    final commit singleton patternt added

--- src/main/java/edo/mex/gob/gui/MainGUI.java
+++ src/main/java/edo/mex/gob/gui/MainGUI.java
@@ -3,9 +3,13 @@ package edo.mex.gob.gui;
 
 import edo.mex.gob.gui.home.HomeGUI;
 
+import edo.mex.gob.repository.BatchSqlTables;
+import java.sql.SQLException;
+
 public class MainGUI {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         new HomeGUI();
+        BatchSqlTables.getInstance();
     }
 }
